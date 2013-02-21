370bed62efd82d7b573814b653bea938e853aac3
David Golden <dagolden@cpan.org>
2013-02-21 08:56:57 -0500

    croak, not die; don't catch file error

--- lib/Acme/require/case.pm
+++ lib/Acme/require/case.pm
@@ -7,6 +7,7 @@ package Acme::require::case;
 # ABSTRACT: Make Perl's require case-sensitive
 # VERSION
 
+use Carp qw/croak/;
 use Path::Tiny;
 use version 0.87;
 
@@ -18,13 +19,13 @@ sub require_casely {
     if ( my $v = eval { version->parse($filename) } ) {
         if ( $v > $^V ) {
             my $which = $v->normal;
-            die "Perl $which required--this is only $^V, stopped";
+            croak "Perl $which required--this is only $^V, stopped";
         }
         return 1;
     }
     if ( exists $INC{$filename} ) {
         return 1 if $INC{$filename};
-        die "Compilation failed in require";
+        croak "Compilation failed in require";
     }
     my ( $realfilename, $result );
     ITER: {
@@ -37,19 +38,19 @@ sub require_casely {
                     last ITER;
                 }
                 else {
-                    die "$filename has incorrect case at $prefix";
+                    croak "$filename has incorrect case at $prefix";
                 }
             }
         }
-        die "Can't find $filename in \@INC (\@INC contains @INC)";
+        croak "Can't find $filename in \@INC (\@INC contains @INC)";
     }
-    if ( $@ || $! ) {
+    if ( $@ ) {
         $INC{$filename} = undef;
-        die $@ ? $@ : $!;
+        croak $@;
     }
     elsif ( !$result ) {
         delete $INC{$filename};
-        die "$filename did not return true value";
+        croak "$filename did not return true value";
     }
     else {
         return $result;
